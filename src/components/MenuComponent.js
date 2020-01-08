ad3440efaaffdccd876a40dcde1a29576c9dec64
kites07 <aniarora2607@gmail.com>
2020-01-08 21:21:06 +0530

    presentational and container

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,69 +1,36 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
-import DishdetailComponent from './DishdetailComponent';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './MenuComponent';
+import DishDetail from './DishdetailComponent';
+import { DISHES } from '../shared/dishes';
 
+class Main extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+        dishes: DISHES,
+        selectedDish: null
+    };
+  }
 
-class Menu extends Component{
+  onDishSelect(dishId) {
+    this.setState({ selectedDish: dishId});
+  }
 
-    constructor(props) {
-        super(props);
-
-        this.state ={
-            selectedDish: null
-        }
-        console.log('menu components constructor is invoked');
-    }
-
-    componentDidMount() {
-        console.log('component did mount is invoked');
-    }
-    onDishSelect(dish) {
-        this.setState({
-            selectedDish: dish
-        });
-    }
-
-   
-
-    render() {
-        const menu = this.props.dishes.map((dish) => {
-            return (
-                <div key={dish.id} className="col-12 col-md-5 m-1">
-                    <Card onClick={() => this.onDishSelect(dish)}>
-                            <CardImg width="100%" src={dish.image} alt={dish.name} />
-                        <CardImgOverlay>
-                            <CardTitle>{dish.name}</CardTitle>
-                           
-                        </CardImgOverlay> 
-                       
-                    </Card>
-                    
-                </div>
-
-            );
-
-        });
-        console.log('menu components render is invoked');
-        return (
-            <div className="container">
-                <div className="row">
-             
-                        {menu}
-                    
-
-                </div>
-                <div className="row">
-                    <DishdetailComponent selectedDish={this.state.selectedDish} />
-                   
-                </div>
-            </div>
-
-
-         );
-    }
+  render() {
+    return (
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
+        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+      </div>
+    );
+  }
 }
 
-
-
-export default Menu;
+export default Main;
